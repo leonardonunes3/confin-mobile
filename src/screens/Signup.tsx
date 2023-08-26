cf2f98aec435bca6343f928be9eb3d6e158555bb
leonardonunes3 <leonardo.nunes793@gmail.com>
2023-08-26 11:35:18 -0300

    signup screen

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,14 +1,72 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View, Image, TouchableOpacity, Text } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../App";
+
+import { Input } from "../components/Input/Input";
+
+type SignUpProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
 
-export function SignUp() {
+export function SignUp({ navigation } : SignUpProps) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
-        <View className="flex-1 justify-center">
-
-            <Text className="">
-                Test
-            </Text>
+        <View className="flex-1 bg-background">
+            <View className="h-1/3 z-0">
+                <Image source={require('../assets/graphs.png')} resizeMode="stretch" className='h-full w-full'/>
+            </View>
+            <View className="bg-background h-3/4 rounded-t-large absolute z-10 bottom-0 left-0 w-full">
+                <View className="h-12 ml-9 mt-20">
+                    <Text className="text-primary font-bold text-3xl">
+                        Cadastro
+                    </Text>
+                </View>
+                <View className="h-12 ml-9 mr-4 mt-8 justify-center">
+                    <Input      
+                        Icon={ <AntDesign name="user" size={20} color="#4A7729" /> } 
+                        onChangeText={setName}
+                        value={name}
+                        placeholder="nome"
+                        placeholderTextColor={'#4A7729'}
+                        secret={false} 
+                    />
+                </View>
+                <View className="h-12 ml-9 mr-4 mt-6 justify-center">
+                    <Input      
+                        Icon={ <MaterialCommunityIcons name="email-outline" size={20} color="#4A7729"/> } 
+                        onChangeText={setEmail}
+                        value={email}
+                        placeholder="email@email.com"
+                        placeholderTextColor={'#4A7729'}
+                        secret={false} 
+                    />
+                </View>
+                <View className="h-12 ml-9 mr-4 mt-6 justify-center">
+                    <Input      
+                        Icon={ <MaterialCommunityIcons name="lock-outline" size={20} color="#4A7729"/> } 
+                        onChangeText={setPassword}
+                        value={password}
+                        placeholder="••••••••••••"
+                        placeholderTextColor={'#4A7729'} 
+                        secret={true}
+                    />
+                </View>
+                <View className="h-12 mx-4 mt-24">
+                    <TouchableOpacity 
+                        className="bg-primary justify-center items-center flex-1 rounded-medium"
+                        activeOpacity={0.7}
+                    >
+                        <Text className="text-white font-regular text-base">
+                            Criar conta
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 }
