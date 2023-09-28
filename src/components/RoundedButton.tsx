ac4b3d7b27ac5f77fb4916e50c8a0d8129e79f77
leonardonunes3 <leonardo.nunes793@gmail.com>
2023-09-28 11:08:12 -0300

    rounded buttons

--- src/components/RoundedButton.tsx
+++ src/components/RoundedButton.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, TextInputProps } from "react-native";
 
 type RoundedButtonProps = {
+    onClick(): void
     description : string,
     extended?: boolean,
     icon : JSX.Element;
@@ -8,15 +9,20 @@ type RoundedButtonProps = {
 
 export function RoundedButton(props : RoundedButtonProps) {
     return(
-        <View className={props.extended ? "w-20" : "w-14"}>
-            <View className="h-12 w-12 rounded-large bg-secondary opacity-95 self-center justify-center items-center">
-                { props.icon }
+        <TouchableOpacity 
+            onPress={props.onClick}
+            activeOpacity={0.7}
+        >
+            <View className={props.extended ? "w-20" : "w-14"}>
+                <View className="h-12 w-12 rounded-large bg-secondary opacity-95 self-center justify-center items-center">
+                    { props.icon }
+                </View>
+                <View>
+                    <Text className="text-primary font-bold text-xs text-center">
+                        { props.description }
+                    </Text>
+                </View>
             </View>
-            <View>
-                <Text className="text-primary font-bold text-xs text-center">
-                    { props.description }
-                </Text>
-            </View>
-        </View>
+        </TouchableOpacity>
     );
 }
